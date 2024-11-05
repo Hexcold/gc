21fbf83d9def2882609776d15acf96958e2c6268
vctorfarias <vfarias3434@gmail.com>
2024-11-05 10:32:12 -0300

    Media Simples : Projeto .c

--- Readme.md
+++ Readme.md
@@ -3,3 +3,6 @@
 ## Atividade em C
 
 $1^a$ - Hello Word
+
+$2^a$ - Média Simples
+
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     print("Hello Word");
+
+    int a = 10, b = 10;
+
+    float mediaSimples = (a + b) / 2;
+
+    printf("A Média é: ", media);
+
     return 0;
 }
